a6e701b3837245611003a2c1b29c01305f723625
Neel Jha <neel.jha@gatech.edu>
2019-03-05 22:50:35 -0500

    WIP

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"golang.org/x/net/html"
+)
+
+func main() {
+	resp, err := http.Get("http://myanimelist.net/users.php")
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer resp.Body.Close()
+	parser := html.NewTokenizer(resp.Body)
+	for {
+		tt := parser.Next()
+		if tt == html.ErrorToken {
+			return
+		}
+		token := parser.Token()
+		if len(token.Attr) > 0 {
+			// [{ href /profile/ReapStick}]
+			fmt.Println(token.Attr)
+		}
+	}
 }
