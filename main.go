3dca97c66d9a0967c853ea16f2d917da6edd5fb1
Neel Jha <neel.jha@gatech.edu>
2019-03-11 23:09:40 -0400

    User discovery queueing

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/google/uuid"
+
+	"github.com/aws/aws-sdk-go/service/sqs"
 
 	"github.com/aws/aws-sdk-go/aws/defaults"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
@@ -19,16 +26,34 @@ const (
 func main() {
 	session := getSession()
 	metricsClient := cloudwatch.New(session)
+	sqsClient := sqs.New(session)
+	queueURL := os.Getenv("QUEUE")
+	if queueURL == "" {
+		panic("Environment variable QUEUE must not be null")
+	}
 	for {
 		users, err := malutil.GetUsers()
 		userCount := 0
 		errCount := 0
+		unqueuedCount := 0
 		if err != nil {
 			errCount++
 		} else {
 			userCount += len(users)
 		}
-		// TODO enque
+		queueMessages := buildQueueMessages(users)
+		for _, batch := range queueMessages {
+			response, err := sqsClient.SendMessageBatch(&sqs.SendMessageBatchInput{
+				QueueUrl: aws.String(queueURL),
+				Entries:  batch,
+			})
+			if err != nil {
+				fmt.Println(err)
+
+			} else {
+				unqueuedCount += len(response.Failed)
+			}
+		}
 
 		// Publish metrics
 		_, err = metricsClient.PutMetricData(&cloudwatch.PutMetricDataInput{
@@ -39,6 +64,12 @@ func main() {
 					MetricName: aws.String("GetUserError"),
 					Unit:       aws.String(cloudwatch.StandardUnitCount),
 					Value:      aws.Float64(float64(errCount)),
+				},
+				// Unqueued user count
+				&cloudwatch.MetricDatum{
+					MetricName: aws.String("PutUserError"),
+					Unit:       aws.String(cloudwatch.StandardUnitCount),
+					Value:      aws.Float64(float64(unqueuedCount)),
 				},
 				// Discovered user count
 				&cloudwatch.MetricDatum{
@@ -64,4 +95,32 @@ func getSession() *session.Session {
 			WithCredentials(defaults.CredChain(config, defaults.Handlers())),
 	))
 	return session
+}
+
+func buildQueueMessages(users []string) [][]*sqs.SendMessageBatchRequestEntry {
+	messages := make([][]*sqs.SendMessageBatchRequestEntry, 0, len(users))
+	batch := make([]*sqs.SendMessageBatchRequestEntry, 0, 10)
+	for _, user := range users {
+		// 10 is the max size of an SQS batch put
+		if len(batch)+1 <= 10 {
+			batch = append(batch, buildBatchRequestEntry(user))
+		} else {
+			messages = append(messages, batch)
+			batch := make([]*sqs.SendMessageBatchRequestEntry, 0, 10)
+			batch = append(batch, buildBatchRequestEntry(user))
+		}
+	}
+	if len(batch) > 0 {
+		messages = append(messages, batch)
+	}
+	return messages
+}
+
+func buildBatchRequestEntry(user string) *sqs.SendMessageBatchRequestEntry {
+	UUID := uuid.New()
+	id := strconv.FormatUint(binary.BigEndian.Uint64(UUID[:]), 16)
+	return &sqs.SendMessageBatchRequestEntry{
+		Id:          aws.String(string(id)),
+		MessageBody: aws.String(user),
+	}
 }
